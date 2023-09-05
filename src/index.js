a3f012655918928f8c7d7bd8ccdd7321c1d19891
Minkyu Kim <min.kim@ocollor.com>
2023-09-05 17:27:09 +0900

    코드 정리, 기능 개선, useCommentFairy와 useShareFairy 플래그 추가

--- src/index.js
+++ src/index.js
@@ -1,38 +1,28 @@
-
-
 (function () {
-  let blogTitle = "";
+  let blogTitle = '';
 
-  let targetQuery = "";
-  let message = "";
-  let linkMessage = "";
-
-  try {
-    targetQuery = fairyTarget;
-  } catch (e) {
-    targetQuery = ".tt-box-write";
-  }
-  try {
-    message = fairyMessage; // 다른 공간에 정의되어 있다고 가정하고 try-catch를 실행
-  } catch (e) {
-    message = "티스토리 계정으로 작성하시겠어요?";
-  }
-
-  try {
-    linkMessage = fairyLinkMessage;
-  } catch (e) {
-    linkMessage = "다시 로그인하기";
-  }
+  const targetQuery =
+    typeof fairyTarget === 'undefined' || fairyTarget === ''
+      ? '.tt-box-write'
+      : fairyTarget;
+  const message =
+    typeof fairyMessage === 'undefined'
+      ? '티스토리 계정으로 작성하시겠어요?'
+      : fairyMessage;
+  const linkMessage =
+    typeof fairyLinkMessage === 'undefined'
+      ? '다시 로그인하기'
+      : fairyLinkMessage;
 
   const observer = new MutationObserver((mutations, observer) => {
     for (let mutation of mutations) {
-      if (mutation.type === "childList") {
+      if (mutation.type === 'childList') {
         // const elementToAdd = document.querySelector(targetQuery).appendChild(fairyBox);
 
         document.querySelectorAll(targetQuery).forEach((e) => {
           if (
-            !e.querySelector(".fairy-messagebox") &&
-            e.querySelector(".tt-btn_register").disabled
+            !e.querySelector('.fairy-messagebox') &&
+            e.querySelector('.tt-btn_register').disabled
           ) {
             addFairy(e);
           }
@@ -44,18 +34,18 @@
   function addFairy(target) {
     observer.disconnect(); // 옵저버 중지
 
-    let num = "";
-    if (typeof ReactionReqBody === "object") {
+    let num = '';
+    if (typeof ReactionReqBody === 'object') {
       num = ReactionReqBody.entryId;
-    } else if (window.location.pathname.indexOf("/guestbook") > -1) {
+    } else if (window.location.pathname.indexOf('/guestbook') > -1) {
       num = 0;
     }
 
-    const fairyBox = document.createElement("div");
-    fairyBox.setAttribute("class", "fairy-messagebox");
+    const fairyBox = document.createElement('div');
+    fairyBox.setAttribute('class', 'fairy-messagebox');
 
-    const textSpan = document.createElement("span");
-    textSpan.setAttribute("class", "fairy-message");
+    const textSpan = document.createElement('span');
+    textSpan.setAttribute('class', 'fairy-message');
 
     // textSpan.innerHTML =
     //     '티스토리 계정으로 댓글을 쓸 수 없나요? <a class="fairy-sign-in" href="https://www.tistory.com/auth/login/?redirectUrl=' +
@@ -68,84 +58,81 @@
       encodeURIComponent(
         TistoryBlog.tistoryUrl +
           window.location.pathname +
-          "#entry" +
+          '#entry' +
           num +
-          "Comment"
+          'Comment'
       ) +
       '" rel="noopener noreferrer">' +
       linkMessage +
-      "</a>";
+      '</a>';
 
     fairyBox.appendChild(textSpan);
-
-    let targetQuery = ".tt-area-reply";
-    try {
-      targetQuery = fairyTarget;
-    } catch (e) {
-      // ignore error, use default target query
-    }
-    document.querySelector(targetQuery);
 
     target.prepend(fairyBox);
     // document.querySelector('.tt-area-reply').appendChild(fairyBox);
     observer.observe(targetNode, config); // 옵저버 재시작
   }
 
-  // 관찰 대상 요소와 설정
-  const targetNode = document.body; // body 전체를 대상으로 설정
-  const config = { childList: true, subtree: true }; // 자식 요소의 추가 및 제거를 관찰하기 위한 설정
+  // 조건부 동작을 유도
+  if (typeof useCommentFairy === 'undefined' || useCommentFairy) {
+    // 관찰 대상 요소와 설정
+    const targetNode = document.body; // body 전체를 대상으로 설정
+    const config = { childList: true, subtree: true }; // 자식 요소의 추가 및 제거를 관찰하기 위한 설정
 
-  observer.observe(targetNode, config);
+    observer.observe(targetNode, config);
+  }
 
-  (function () {
-    function isShortContents() {
-      return window.getSelection().toString().length < 30;
-    }
-
-    function copyWithSource2(event) {
-      if (isShortContents()) {
-        return;
+  if (typeof useShareFairy === 'undefined' || useShareFairy) {
+    (function () {
+      function isShortContents() {
+        return window.getSelection().toString().length < 30;
       }
-      var range = window.getSelection().getRangeAt(0);
-      var contents = range.cloneContents();
-      var temp = document.createElement("div");
-
-      temp.appendChild(contents);
-
-      //var url = document.location.href;
-      let host = "";
-
-      try {
-        host += TistoryBlog.url;
-      } catch (e) {
-        host += document.location.domainname;
+  
+      function copyWithSource2(event) {
+        if (isShortContents()) {
+          return;
+        }
+        var range = window.getSelection().getRangeAt(0);
+        var contents = range.cloneContents();
+        var temp = document.createElement('div');
+  
+        temp.appendChild(contents);
+  
+        //var url = document.location.href;
+        let host = '';
+  
+        try {
+          host += TistoryBlog.url;
+        } catch (e) {
+          host += document.location.domainname;
+        }
+  
+        const innerText = temp.innerText || document.title;
+  
+        var url = host + document.location.pathname;
+        var decodedUrl = decodeURI(url);
+        var postfix = ''; // " [" + blogTitle + ":티스토리]"; // blog title의 값을 구할 방법이 없는 관계로 보류
+  
+        event.clipboardData.setData(
+          'text/plain',
+          innerText + '\n출처: ' + decodedUrl + postfix
+        );
+        event.clipboardData.setData(
+          'text/html',
+          '&lt;pre data-ke-type=&quot;codeblock&quot;&gt;' +
+            temp.innerHTML +
+            '&lt;/pre&gt;' +
+            '출처: &lt;a href=&quot;' +
+            url +
+            '&quot;&gt;' +
+            decodedUrl +
+            '&lt;/a&gt;' +
+            postfix
+        );
+        event.preventDefault();
       }
-
-      const innerText = temp.innerText || document.title;
-
-      var url = host + document.location.pathname;
-      var decodedUrl = decodeURI(url);
-      var postfix = ''; // " [" + blogTitle + ":티스토리]"; // blog title의 값을 구할 방법이 없는 관계로 보류
-
-      event.clipboardData.setData(
-        "text/plain",
-        innerText + "\n출처: " + decodedUrl + postfix
-      );
-      event.clipboardData.setData(
-        "text/html",
-        "&lt;pre data-ke-type=&quot;codeblock&quot;&gt;" +
-          temp.innerHTML +
-          "&lt;/pre&gt;" +
-          "출처: &lt;a href=&quot;" +
-          url +
-          "&quot;&gt;" +
-          decodedUrl +
-          "&lt;/a&gt;" +
-          postfix
-      );
-      event.preventDefault();
-    }
-
-    document.addEventListener("copy", copyWithSource2);
-  })();
+  
+      document.addEventListener('copy', copyWithSource2);
+    })();
+  }
 })();
